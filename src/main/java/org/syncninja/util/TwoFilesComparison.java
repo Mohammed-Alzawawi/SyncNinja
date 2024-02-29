1eb2ae1685532385a8bde827f8f69d38cffa5cd5
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-02-29 19:31:33 +0300

    fix new lines

--- src/main/java/org/syncninja/util/TwoFilesComparison.java
+++ src/main/java/org/syncninja/util/TwoFilesComparison.java
@@ -3,10 +3,7 @@ package org.syncninja.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class TwoFilesComparison {
 
@@ -14,7 +11,7 @@ public class TwoFilesComparison {
         try (BufferedReader reader1 = new BufferedReader(new FileReader(newFilePath));
              BufferedReader reader2 = new BufferedReader(new FileReader(oldFilePath))) {
 
-            ArrayList<String[]> differanceList= new ArrayList<>();
+            ArrayList<String[]> differenceList= new ArrayList<>();
             String[] tempList;
 
             int lineNumber = 1;
@@ -22,11 +19,25 @@ public class TwoFilesComparison {
             while ((newLine = reader1.readLine()) != null && (oldLine = reader2.readLine()) != null) {
                 if (!newLine.equals(oldLine)) {
                     tempList = new String[]{String.valueOf(lineNumber),newLine, oldLine};
-                    differanceList.add(tempList);
+                    differenceList.add(tempList);
                 }
                 lineNumber++;
             }
-            return differanceList;
+
+            while ((newLine = reader1.readLine()) != null) {
+                tempList = new String[]{String.valueOf(lineNumber), newLine, ""};
+                differenceList.add(tempList);
+                lineNumber++;
+            }
+
+            // Handle remaining lines in old file
+            while ((oldLine = reader2.readLine()) != null) {
+                tempList = new String[]{String.valueOf(lineNumber), "", oldLine};
+                differenceList.add(tempList);
+                lineNumber++;
+            }
+
+            return differenceList;
         }
     }
 }
