49ea885fa0875cfaa3550d7b570427839d8aa08b
LeenJaber1 <jaberleen95@gmail.com>
2024-03-01 14:43:07 +0300

    last commit for status command to get the untracked files

--- src/main/java/org/syncninja/command/InitCommand.java
+++ src/main/java/org/syncninja/command/InitCommand.java
@@ -3,12 +3,14 @@ package org.syncninja.command;
 import org.syncninja.model.Directory;
 import org.syncninja.service.DirectoryService;
 import org.syncninja.service.ResourceMessagingService;
+import org.syncninja.service.StateTreeService;
 import org.syncninja.util.ResourceBundleEnum;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "init")
 public class InitCommand implements Runnable {
     private final DirectoryService directoryService;
+    private final StateTreeService stateTreeService = new StateTreeService();
 
     public InitCommand() {
         this.directoryService = new DirectoryService();
@@ -20,6 +22,7 @@ public class InitCommand implements Runnable {
         try {
             Directory directory = directoryService.createDirectory(path);
             directoryService.createDirectoryMainBranch(directory, "main");
+            stateTreeService.generateStateDirectoryNode(path);
             System.out.println(ResourceMessagingService.getMessage(ResourceBundleEnum.DIRECTORY_INITIALIZED_SUCCESSFULLY, new Object[]{path}));
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
