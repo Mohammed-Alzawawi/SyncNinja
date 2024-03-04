fcc694a465f8362f5312a0334f4eca9ef014cdf9
Tala <talaqudah86@gmail.com>
2024-03-04 13:42:10 +0300

    add options

--- src/main/java/org/syncninja/service/CommitTreeService.java
+++ src/main/java/org/syncninja/service/CommitTreeService.java
@@ -5,7 +5,12 @@ import org.syncninja.model.commitTree.CommitFile;
 import org.syncninja.model.commitTree.CommitNode;
 import org.syncninja.repository.CommitNodeRepository;
 import org.syncninja.util.FileState;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 
 public class CommitTreeService {
@@ -20,6 +25,33 @@ public class CommitTreeService {
         FileState fileState = statusService.getStatus(directoryPath);
         List<String> untracedFiles =fileState.getUntracked();
         addFilesToCommitTree(untracedFiles,directoryPath);
+    }
+    //all
+    public void addDirectoryToCommitTree(String directoryPath) throws IOException {
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException("The provided path is not a directory: " + directoryPath);
+        }
+        List<String> filePaths = Files.walk(Path.of(directoryPath))
+                .filter(Files::isRegularFile)
+                .map(Path::toString)
+                .toList();
+
+        addFilesToCommitTree(filePaths, directoryPath);
+
+        // (/user)
+        File[] subDirectories = directory.listFiles(File::isDirectory);
+        if (subDirectories != null) {
+            for (File subDirectory : subDirectories) {
+                addDirectoryToCommitTree(subDirectory.getAbsolutePath());
+            }
+        }
+    }
+    public void addFileToCommitTree(String filePath) {
+        if (!Files.isRegularFile(Path.of(filePath))) {
+            throw new IllegalArgumentException("The provided path is not a regular file: " + filePath);
+        }
+        addFilesToCommitTree(Collections.singletonList(filePath), new File(filePath).getParent());
     }
     private void addFilesToCommitTree(List<String> filePaths, String mainDirectoryPath) {
         CommitNode root = new CommitDirectory(mainDirectoryPath);
@@ -59,4 +91,6 @@ public class CommitTreeService {
     private boolean isFile(String path) {
         return new File(path).isFile();
     }
+
 }
+
