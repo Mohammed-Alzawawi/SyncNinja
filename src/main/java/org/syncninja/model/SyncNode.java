f859763a3e3877de90c90ab3ce1dd1707161365b
NadiaAlshariah <nadiakalshariah@gmail.com>
2024-03-13 15:49:24 +0300

    add creationTime

--- src/main/java/org/syncninja/model/SyncNode.java
+++ src/main/java/org/syncninja/model/SyncNode.java
@@ -2,18 +2,24 @@ package org.syncninja.model;
 
 import org.neo4j.ogm.annotation.Id;
 
+import java.util.Date;
 import java.util.UUID;
 
 public abstract class SyncNode {
-
     @Id
     private final String id;
+    private final Date creationTime;
 
     public SyncNode() {
         this.id = UUID.randomUUID().toString();
+        this.creationTime = new Date();
     }
 
     public String getId() {
         return id;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
     }
 }
